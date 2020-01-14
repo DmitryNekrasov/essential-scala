e2dc5e0042d5fe1d767dffb3cab220d3adf1c4d0
Dmitry Nekrasov <scannorone@gmail.com>
2020-01-14 14:29:54 +0700

    Product

--- src/main/scala/IntListExample.scala
+++ src/main/scala/IntListExample.scala
@@ -3,12 +3,20 @@ import scala.annotation.tailrec
 object IntListExample extends App {
   sealed trait IntList {
     final def length: Int = length(0)
+    final def product: Int = product(1)
 
     @tailrec
     private def length(accum: Int): Int =
       this match {
         case End => accum
         case Pair(_, tail) => tail.length(accum + 1)
+      }
+
+    @tailrec
+    private def product(accum: Int): Int =
+      this match {
+        case End => accum
+        case Pair(head, tail) => tail.product(accum * head)
       }
   }
   case object End extends IntList
@@ -20,5 +28,10 @@ object IntListExample extends App {
   assert(list.tail.length == 2)
   assert(End.length == 0)
 
+  assert(list.product == 6)
+  assert(list.tail.product == 6)
+  assert(End.product == 1)
+
   println(list.length)
+  println(list.product)
 }

2ff59bbab7a9aa6fe71577f575b747fb68a80092
Dmitry Nekrasov <scannorone@gmail.com>
2020-01-17 16:34:46 +0700

    Sum

--- src/main/scala/Generics.scala
+++ src/main/scala/Generics.scala
@@ -1,23 +1,23 @@
 object Generics extends App {
   final case class Pair[A, B](one: A, two: B)
 
-  sealed trait Sum[A, B] {
+  sealed trait Sum[+A, +B] {
     final def fold[C](failure: A => C)(success: B => C): C =
       this match {
         case Failure(a) => failure(a)
         case Success(b) => success(b)
       }
 
-    final def map[C](f: B => C) : Sum[A, C] = flatMap[C](value => Success(f(value)))
+    final def map[C](f: B => C) : Sum[A, C] = flatMap[A, C](value => Success(f(value)))
 
-    final def flatMap[C](f: B => Sum[A, C]): Sum[A, C] =
+    final def flatMap[AA >: A, C](f: B => Sum[AA, C]): Sum[AA, C] =
       this match {
         case Failure(a) => Failure(a)
         case Success(b) => f(b)
       }
   }
-  final case class Failure[A, B](value: A) extends Sum[A, B]
-  final case class Success[A, B](value: B) extends Sum[A, B]
+  final case class Failure[A](value: A) extends Sum[A, Nothing]
+  final case class Success[B](value: B) extends Sum[Nothing, B]
 
   sealed trait Maybe[A] {
     final def fold[B](empty: B)(full: A => B): B =
@@ -43,13 +43,13 @@ object Generics extends App {
   println(pair.one)
   println(pair.two)
 
-  val l1 = Failure[Int, String](1).value
+  val l1 = Failure[Int](1).value
   println(l1)
 
-  val r1 = Success[Int, String]("foo").value
+  val r1 = Success[String]("foo").value
   println(r1)
 
-  val sum: Sum[Int, String] = Success("foo")
+  val sum: Sum[Any, String] = Success("foo")
 
   val result = sum match {
     case Failure(value) => value.toString
